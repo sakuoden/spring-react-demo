d7e79a1d1b70fbc0a0464789a6d685667818172b
sakuoden <4oden18@gmail.com>
2022-03-13 02:30:25 +0900

    import文の省略を無くす

--- src/main/java/jtn/demo/presentation/DemoController.java
+++ src/main/java/jtn/demo/presentation/DemoController.java
@@ -1,6 +1,10 @@
 package jtn.demo.presentation;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("demo")
